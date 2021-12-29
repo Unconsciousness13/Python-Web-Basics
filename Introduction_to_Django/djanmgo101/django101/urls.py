bd84bb14680ee503290f3c43eff570a7d4b6ab27
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2021-12-29 12:08:58 +0100

    Django

--- Introduction_to_Django/djanmgo101/django101/urls.py
+++ Introduction_to_Django/djanmgo101/django101/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from django101.views import index
+from django101.views import index, test 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('test/', test),
 ]
--- Introduction_to_Django/djanmgo101/django101/views.py
+++ Introduction_to_Django/djanmgo101/django101/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(req):
     return render(req, 'index.html')
+
+def test(req):
+    return render(req, 'test.html')
