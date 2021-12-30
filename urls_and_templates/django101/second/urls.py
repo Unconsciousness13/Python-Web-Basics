c36bf8aaca3d473abca5aa651f4b975409d499a0
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2021-12-30 19:01:40 +0100

    Django

--- urls_and_templates/django101/second/urls.py
+++ urls_and_templates/django101/second/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from second.phones.views import index, phones_list
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('phones/', phones_list)
 ]
