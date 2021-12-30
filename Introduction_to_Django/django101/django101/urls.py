7dd39c9bc30c8e0c2cb88d5624c82bdacf849141
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2021-12-30 18:46:04 +0100

    Django

--- Introduction_to_Django/django101/django101/urls.py
+++ Introduction_to_Django/django101/django101/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from django101.cities.views import index, test
+from  django101.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path('test/', test),
 ]
+
--- urls_and_templates/django101/second/phones/apps.py
+++ urls_and_templates/django101/second/phones/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CitiesConfig(AppConfig):
+class PhonesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cities'
+    name = 'phones'
