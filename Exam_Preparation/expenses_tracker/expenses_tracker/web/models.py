0d5318a6665da9aa76551f722d5c748e656162c1
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-02-17 13:42:50 +0100

    Exam prep

--- Exam_Preparation/expenses_tracker/expenses_tracker/web/models.py
+++ Exam_Preparation/expenses_tracker/expenses_tracker/web/models.py
@@ -1,9 +1,32 @@
+from django.core.exceptions import ValidationError
 from django.core.validators import MinLengthValidator, MinValueValidator
 from django.db import models
+from django.utils.deconstruct import deconstructible
+
+VALIDATE_ONLY_LETTERS_EXCEPTION_MESSAGE = 'Ensure this value contains only letters.'
 
 
 def validate_only_letters(value):
-    pass
+    if not value.isalpha():
+        raise ValidationError(VALIDATE_ONLY_LETTERS_EXCEPTION_MESSAGE)
+
+
+@deconstructible
+class MaxFileSizeInMbValidator:
+    def __init__(self, max_size):
+        self.max_size = max_size
+
+    def __call__(self, value):
+        filesize = value.file.size
+        if filesize > self.__megabytes_to_bytes(self.max_size):
+            raise ValidationError(self.__get_exception_message())
+
+    @staticmethod
+    def __megabytes_to_bytes(value):
+        return value * 1024 * 1024
+
+    def __get_exception_message(self):
+        return f'Max file size is {self.max_size:.2f} MB'
 
 
 class Profile(models.Model):
@@ -15,6 +38,9 @@ class Profile(models.Model):
 
     BUDGET_DEFAULT_VALUE = 0
     BUDGET_MIN_VALUE = 0
+
+    IMAGE_MAX_SIZE_IN_MB = 5
+    IMAGE_UPLOAD_TI_DIR = 'profiles/'
 
     first_name = models.CharField(
         max_length=FIRST_NAME_MAX_LEN,
@@ -34,12 +60,17 @@ class Profile(models.Model):
     budget = models.FloatField(
         default=BUDGET_DEFAULT_VALUE,
         validators=(
-            MinValueValidator(BUDGET_MIN_VALUE)
+            MinValueValidator(BUDGET_MIN_VALUE),
         ),
     )
 
     profile_image = models.ImageField(
-
+        upload_to=IMAGE_UPLOAD_TI_DIR,
+        null=True,
+        blank=True,
+        validators=(
+            MaxFileSizeInMbValidator(IMAGE_MAX_SIZE_IN_MB),
+        )
     )
 
 
