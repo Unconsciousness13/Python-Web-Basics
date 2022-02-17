01a980e70852ae85c7227c9f7dae74ffb7563319
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-02-17 10:44:40 +0100

    Exam prep

--- Exam_Preparation/expenses_tracker/expenses_tracker/web/views.py
+++ Exam_Preparation/expenses_tracker/expenses_tracker/web/views.py
@@ -3,28 +3,28 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_index(request):
-    pass
+    return render(request, 'home-with-profile.html')
 
 
 def create_expense(request):
-    pass
+    return render(request, 'expense-create.html')
 
 
 def edit_expense(request):
-    pass
+    return render(request, 'expense-edit.html')
 
 
 def delete_expense(request):
-    pass
+    return render(request, 'expense-delete.html')
 
 
 def show_profile(request):
-    pass
+    return render(request, 'profile.html')
 
 
 def edit_profile(request):
-    pass
+    return render(request, 'profile-edit.html')
 
 
 def delete_profile(request):
-    pass
+    return render(request, 'profile-delete.html')
