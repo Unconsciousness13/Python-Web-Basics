e54ee83e1518b470f13ce42e346add8dc0a170d3
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-02-17 13:01:57 +0100

    Exam prep

--- Exam_Preparation/expenses_tracker/expenses_tracker/web/models.py
+++ Exam_Preparation/expenses_tracker/expenses_tracker/web/models.py
@@ -1,3 +1,47 @@
+from django.core.validators import MinLengthValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+def validate_only_letters(value):
+    pass
+
+
+class Profile(models.Model):
+    FIRST_NAME_MIN_LEN = 2
+    FIRST_NAME_MAX_LEN = 15
+
+    LAST_NAME_MIN_LEN = 2
+    LAST_NAME_MAX_LEN = 15
+
+    BUDGET_DEFAULT_VALUE = 0
+    BUDGET_MIN_VALUE = 0
+
+    first_name = models.CharField(
+        max_length=FIRST_NAME_MAX_LEN,
+        validators=(
+            MinLengthValidator(FIRST_NAME_MIN_LEN),
+            validate_only_letters,
+        )
+    )
+    last_name = models.CharField(
+        max_length=LAST_NAME_MAX_LEN,
+        validators=(
+            MinLengthValidator(LAST_NAME_MIN_LEN),
+            validate_only_letters,
+        )
+    )
+
+    budget = models.FloatField(
+        default=BUDGET_DEFAULT_VALUE,
+        validators=(
+            MinValueValidator(BUDGET_MIN_VALUE)
+        ),
+    )
+
+    profile_image = models.ImageField(
+
+    )
+
+
+class Expense(models.Model):
+    pass
